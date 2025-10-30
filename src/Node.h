d6de745edb1540ba475d59d8c5d72831eae135b4
David Castrejon <david.j.castrejon@gmail.com>
2025-10-30 01:23:22 -0400

    Add initial Node class

---
+++ src/Node.h
@@ -0,0 +1,35 @@
+// Does the same as '#ifndef NODE_H ... #define NODE_H ... #endif'
+// Compiler tracks physical file intead of being text based like #ifndef
+// Avoids potential naming issues with dupilicated file names like src/Node.h and util/Node.h 
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Node {
+// Node and derived classes can access
+protected:
+    std::string d_id;
+    std::vector<Node*> d_parents;
+    std::vector<Node*> d_children;
+
+    // Prevents implicit converion
+    // Typically always use for single-parameter constructors
+    explicit Node(const std::string& id) : d_id(id) {}
+
+// Everyone can access
+public:
+    // Allows derived classes to overide base class
+    virtual ~Node() = default;
+
+    // Pure virtual - must be implemented by derived class
+    virtual std::string getType() const = 0;
+
+    const std::string& const id() { return d_id; }
+    const std::vector<Node*>& parents() { return d_parents; }
+    const std::vector<Node*>& children() { return d_children; }
+
+// Node can access
+private:
+
+};
